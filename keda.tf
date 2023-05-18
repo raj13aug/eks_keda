5c375a27e77b095b9c4bab16241c53136857c808
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-05-18 14:23:28 +0530

    Update keda.tf

--- keda.tf
+++ keda.tf
@@ -74,3 +74,28 @@ module "irsa_keda" {
     local.tags
   )
 }
+
+resource "kubernetes_namespace" "keda" {
+  depends_on = [var.mod_dependency]
+  count      = (var.enabled && var.create_namespace && var.namespace != "kube-system") ? 1 : 0
+
+  metadata {
+    name = var.namespace
+  }
+}
+
+
+resource "helm_release" "keda" {
+  depends_on = [var. , kubernetes_namespace.keda]
+  count      = var.enabled ? 1 : 0
+  name       = var.helm_chart_name
+  chart      = var.helm_chart_release_name
+  repository = var.helm_chart_repo
+  version    = var.helm_chart_version
+  namespace  = var.namespace
+
+
+  values = [
+    yamlencode(var.settings)
+  ]
+}
