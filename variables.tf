ea3e8040f7c083313662f29b0d223b79ef86c28a
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-05-18 14:52:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,16 +7,18 @@ variable "cluster_name" {
 variable "namespace" {
   type        = string
   description = "The Kubernetes namespace"
+  default     = "keda"
 }
 
 variable "service_account" {
   type        = string
   description = "The Kubernetes service account"
+  default     = "keda-operator"
 }
 
 variable "tags" {
   type        = map(string)
-  description = "Tags for Loki"
+  description = "Tags for Nataraj"
   default = {
   }
 }
